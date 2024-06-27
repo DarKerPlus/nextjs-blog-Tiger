7f13e69d3d5f07d9b98b8a115e3fcd2e6332908c
liubang <107918242+DarKerPlus@users.noreply.github.com>
2024-06-27 14:19:31 +0800

    14:19

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,11 @@ import SEO from '../components/SEO';
 export default function Index({ posts, globalData }) {
   return (
     <Layout>
-      <SEO title={globalData.name} description={globalData.blogTitle} />
+      <SEO title='tiger' description={globalData.blogTitle} />
       <Header name={globalData.name} />
       <main className="w-full">
         <h1 className="text-3xl lg:text-5xl text-center mb-12">
-          {globalData.blogTitle}
+          'good site tiger'
         </h1>
         <ul className="w-full">
           {posts.map((post) => (
