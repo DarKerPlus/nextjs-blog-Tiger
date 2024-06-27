ad71c11d6eb833936bf6e7718f26072b837ed753
liubang <107918242+DarKerPlus@users.noreply.github.com>
2024-06-27 14:21:27 +0800

    14:21

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Index({ posts, globalData }) {
       <Header name={globalData.name} />
       <main className="w-full">
         <h1 className="text-3xl lg:text-5xl text-center mb-12">
-          'good site tiger'
+          good site tiger
         </h1>
         <ul className="w-full">
           {posts.map((post) => (
